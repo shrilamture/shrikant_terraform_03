177b106085bf0956672451e5f1feff9c8810f6c2
shrilamture <91794701+shrilamture@users.noreply.github.com>
2023-04-03 13:18:27 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,16 @@
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/24"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "main"
+  }
+}
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "Main"
+  }
+}
